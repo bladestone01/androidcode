0cf75e70e3648738f314a64907f40a2105e585cc
bladestone <65040491@qq.com>
2024-07-18 16:33:54 +0800

    init the notebook project

--- V1/chapter6/notebook/app/src/main/java/org/test/chapter06/notebook/ResultActivity.java
+++ V1/chapter6/notebook/app/src/main/java/org/test/chapter06/notebook/ResultActivity.java
@@ -17,7 +17,6 @@ public class ResultActivity extends Activity {
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.popup);
